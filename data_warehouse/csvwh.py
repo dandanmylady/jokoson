7afa0dcbfa28a41bb92dc35b246c6a23edd91cd1
lyndon Zhang <lyndon.zhang@qq.com>
2016-11-30 22:56:13 +0800

    This version of csv warehouse passed many testing, but it's too tedious. It's not necessary to consider 'buffer'.  just read/write all data at once.

--- data_warehouse/csvwh.py
+++ data_warehouse/csvwh.py
@@ -9,6 +9,7 @@ class CsvWarehouse(Warehouse):
 
     Note:
         1. It's not thread-safe.  Don't get/add records in different threads.
+        2. The fieldnames must be the same as the csv file, even the order.
     """
     def __init__(self, path, fieldnames, delimiter=';'):
         self.filePath = path
@@ -24,66 +25,70 @@ class CsvWarehouse(Warehouse):
         self.readRecordsWithError = []
         self.writeRecordsWithError = []
         self.magicWorldForNonexist = 'youshouldnotbehere'
+        try:
+            self.readHandler = open(self.filePath)
+        except IOError:
+            self.readHandler = open(self.filePath, 'w+')
+
+    def __del__(self):
+        self.flush()
+        if self.readHandler:
+            self.readHandler.close()
+
+    # This is a little bit ugly. If the application panic before calling flush, Data lost happens.
+    # TODO: find a robust way to avoid data lost
+    def flush(self):
+        if len(self.writeBuffer) > 0:
+            self._write(self.writeBuffer)
+            self.writeBuffer = []
 
     # This method really read the file and never read the buffer
     def _read(self, count):
-        if count <= 0:
+        if count <= 0 or self._isFileEmpty():
             return []
 
         res = []
-        with open(self.filePath) as csvfile:
-            csvfile.seek(self.readSeek)
-            reader = csv.DictReader(csvfile, fieldnames=self.fieldNames, restkey=self.magicWorldForNonexist,
-                                    restval=self.magicWorldForNonexist)
-            if not self.doesHeaderChecked:
+        reader = csv.DictReader(self.readHandler, fieldnames=self.fieldNames, restkey=self.magicWorldForNonexist,
+                                restval=self.magicWorldForNonexist)
+        if not self.doesHeaderChecked:
+            head = reader.next()
+            if head.keys().sort() != self.fieldNames.sort() or self.magicWorldForNonexist in head.values():
+                raise Exception("The CSV file's format is not exactly the same as the fieldnames")
+            elif head.values().sort() == self.fieldNames.sort():
+                # This line is the header , just ignore it.
+                # TODO: Should convert all values to lowercase, before comparing.
+                self.doesHeaderChecked = True
+            else:
+                self.doesHeaderChecked = True
+                self.totalReadCount += 1
                 count -= 1
-                head = reader.next()
-                if head.keys().sort() != self.fieldNames.sort() or self.magicWorldForNonexist in head.values():
-                    raise Exception("The CSV file's format is not exactly the same as the fieldnames")
-                elif head.values().sort() == self.fieldNames.sort():
-                    # This line is the header , just ignore it.
-                    # TODO: Should convert all values to lowercase, before comparing.
-                    self.doesHeaderChecked = True
-                else:
-                    self.doesHeaderChecked = True
-                    self.totalReadCount += 1
-                    res.append(head)
+                res.append(head)
 
-            i = 0
-            try:
-                while i < count:
-                    res.append(reader.next())
-                    self.totalReadCount += 1
-                    i += 1
-            except StopIteration:
-                pass
-            finally:
-                self.readSeek = csvfile.tell()
+        i = 0
+        try:
+            while i < count:
+                res.append(reader.next())
+                self.totalReadCount += 1
+                i += 1
+        except StopIteration:
+            pass
 
         return res
 
-    def getRecord(self, count=1):
+    def getRecord(self, count=1024):
+        if self._isFileEmpty():
+            return []
         if len(self.readBuffer) >= count:
             res = self.readBuffer[0:count]
             del self.readBuffer[0:count]
             return res
         else:
             res = self.readBuffer
-            self.readBuffer = []
-            if self._isFileEmpty():
-                return res
-
             self.readBuffer = self._read(self.maxBufferLength)
-
-            if len(res) + len(self.readBuffer) >= count:
-                res += self.readBuffer[0:count - len(res)]
-                del self.readBuffer[0:count - len(res)]
+            if len(self.readBuffer) == 0:
                 return res
             else:
-                res += self.readBuffer
-                self.readBuffer = []
-                res += self.getRecord(count - len(res))
-                return res
+                return res + self.getRecord(count - len(res))
 
     def _isFileEmpty(self):
         return (not os.path.isfile(self.filePath)) or os.path.getsize(self.filePath) == 0
@@ -140,15 +145,22 @@ def _testWrite():
     i = 0
     while i <= 10:
         records.append({'Equip':'Shit', 'User':'SuperMan', 'Count':100 + i})
+        i += 1
+    print(len(records))
     csvwh.addRecord(records)
-
+    print (csvwh.totalWriteCount)
 
 def _testRead():
     csvwh = CsvWarehouse("./test1.csv", ['Equip', 'User', 'Count'])
     csvwh.maxBufferLength = 5
-    res = csvwh.getRecord(10)
+    res = csvwh.getRecord(9)
     for r in res:
         print("equip {}, User {}, Count {}".format(r['Equip'], r['User'], r['Count']))
+    res = csvwh.getRecord(9)
+    for r in res:
+        print("equip {}, User {}, Count {}".format(r['Equip'], r['User'], r['Count']))
+    print (csvwh.totalReadCount)
 
 if __name__ == "__main__":
+    _testWrite()
     _testRead()
